f17760225b8a7f9db7df268e143279b8fe6f11d4
Peter Gribanov <wcode404@gmail.com>
2016-09-29 17:18:52 +0300

    test Bus

---
+++ tests/Bus/BusTest.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2016, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+namespace GpsLab\Domain\Event\Tests\Bus;
+
+use GpsLab\Domain\Event\Aggregator\AggregateEventsInterface;
+use GpsLab\Domain\Event\Bus\Bus;
+use GpsLab\Domain\Event\EventInterface;
+use GpsLab\Domain\Event\Listener\ListenerInterface;
+use GpsLab\Domain\Event\Listener\Locator\LocatorInterface;
+
+class BusTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|LocatorInterface
+     */
+    private $locator;
+
+    /**
+     * @var Bus
+     */
+    private $bus;
+
+    protected function setUp()
+    {
+        $this->locator = $this->getMock(LocatorInterface::class);
+        $this->bus = new Bus($this->locator);
+    }
+
+    public function testPublish()
+    {
+        /* @var $event \PHPUnit_Framework_MockObject_MockObject|EventInterface */
+        $event = $this->getMock(EventInterface::class);
+
+        /* @var $listeners \PHPUnit_Framework_MockObject_MockObject[] */
+        $listeners = [
+            $this->getMock(ListenerInterface::class),
+            $this->getMock(ListenerInterface::class),
+        ];
+
+        foreach ($listeners as $listener) {
+            $listener
+                ->expects($this->once())
+                ->method('handle')
+                ->with($event);
+        }
+
+        $this->locator
+            ->expects($this->once())
+            ->method('getListenersForEvent')
+            ->with($event)
+            ->will($this->returnValue($listeners));
+
+        $this->bus->publish($event);
+    }
+
+    public function testPullAndPublish()
+    {
+        /* @var $events \PHPUnit_Framework_MockObject_MockObject[] */
+        $events = [
+            $this->getMock(EventInterface::class),
+            $this->getMock(EventInterface::class),
+        ];
+
+        foreach ($events as $i => $event) {
+            /* @var $listeners \PHPUnit_Framework_MockObject_MockObject[] */
+            $listeners = [
+                $this->getMock(ListenerInterface::class),
+                $this->getMock(ListenerInterface::class),
+            ];
+
+            foreach ($listeners as $listener) {
+                $listener
+                    ->expects($this->once())
+                    ->method('handle')
+                    ->with($event);
+            }
+
+            $this->locator
+                ->expects($this->at($i))
+                ->method('getListenersForEvent')
+                ->with($event)
+                ->will($this->returnValue($listeners));
+        }
+
+        /* @var $aggregator \PHPUnit_Framework_MockObject_MockObject|AggregateEventsInterface */
+        $aggregator = $this->getMock(AggregateEventsInterface::class);
+        $aggregator
+            ->expects($this->once())
+            ->method('pullEvents')
+            ->will($this->returnValue($events));
+
+        $this->bus->pullAndPublish($aggregator);
+    }
+
+    public function testGetRegisteredEventListeners()
+    {
+        $listeners = [
+            $this->getMock(ListenerInterface::class),
+        ];
+
+        $this->locator
+            ->expects($this->once())
+            ->method('getRegisteredEventListeners')
+            ->will($this->returnValue($listeners));
+
+        $this->assertEquals($listeners, $this->bus->getRegisteredEventListeners());
+    }
+}
