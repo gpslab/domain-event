e7523ca5a1216fa851c78327f6b2f556b8b23387
Peter Gribanov <wcode404@gmail.com>
2016-09-29 18:02:51 +0300

    test AggregateEvents

---
+++ tests/Aggregator/AggregateEventsTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2016, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+namespace GpsLab\Domain\Event\Tests\Aggregator;
+
+use GpsLab\Domain\Event\EventInterface;
+
+class AggregateEventsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var DemoAggregator
+     */
+    private $aggregator;
+
+    protected function setUp()
+    {
+        $this->aggregator = new DemoAggregator();
+    }
+
+    public function testRaiseAndPullEvents()
+    {
+        $this->assertEquals([], $this->aggregator->pullEvents());
+
+        $events = [
+            $this->getMock(EventInterface::class),
+            $this->getMock(EventInterface::class),
+        ];
+
+        foreach ($events as $event) {
+            $this->aggregator->raiseEvent($event);
+        }
+
+        $this->assertEquals($events, $this->aggregator->pullEvents());
+        $this->assertEquals([], $this->aggregator->pullEvents());
+    }
+}
---
+++ tests/Aggregator/DemoAggregator.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2016, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+namespace GpsLab\Domain\Event\Tests\Aggregator;
+
+use GpsLab\Domain\Event\Aggregator\AbstractAggregateEvents;
+use GpsLab\Domain\Event\EventInterface;
+
+class DemoAggregator extends AbstractAggregateEvents
+{
+    /**
+     * @param EventInterface $event
+     */
+    public function raiseEvent(EventInterface $event)
+    {
+        $this->raise($event);
+    }
+}
