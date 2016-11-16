c651c5a8be358361f66a1639e7861be54b0fbcfd
Peter Gribanov <wcode404@gmail.com>
2016-11-16 15:47:43 +0300

    create ContainerAwareLocator

---
+++ src/Listener/Locator/ContainerAwareLocator.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Pkvs package
+ *
+ * @package Pkvs
+ * @author  Peter Gribanov <pgribanov@1tv.com>
+ */
+namespace GpsLab\Domain\Event\Listener\Locator;
+
+use GpsLab\Domain\Event\EventInterface;
+use GpsLab\Domain\Event\Listener\ListenerCollection;
+use GpsLab\Domain\Event\Listener\ListenerInterface;
+use GpsLab\Domain\Event\NameResolver\EventNameResolverInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ContainerAwareLocator extends NamedEventLocator
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @var EventNameResolverInterface
+     */
+    private $resolver;
+
+    /**
+     * @var array
+     */
+    private $listener_ids = [];
+
+    /**
+     * @var array
+     */
+    private $listeners = [];
+
+    /**
+     * @param EventNameResolverInterface $resolver
+     * @param ContainerInterface $container
+     */
+    public function __construct(EventNameResolverInterface $resolver, ContainerInterface $container)
+    {
+        parent::__construct($resolver);
+        $this->container = $container;
+        $this->resolver = $resolver;
+    }
+
+    /**
+     * @param string $event_name
+     * @param string $service
+     */
+    public function registerService($event_name, $service)
+    {
+        $this->listener_ids[$event_name][] = $service;
+    }
+
+    /**
+     * @param EventInterface $event
+     *
+     * @return ListenerInterface[]
+     */
+    public function getListenersForEvent(EventInterface $event)
+    {
+        $event_name = $this->resolver->getEventName($event);
+        $this->lazyLoad($event_name);
+
+        return parent::getListenersForEvent($event);
+    }
+
+    /**
+     * @return ListenerCollection
+     */
+    public function getRegisteredEventListeners()
+    {
+        foreach ($this->listener_ids as $event_name => $service) {
+            $this->lazyLoad($event_name);
+        }
+
+        return parent::getRegisteredEventListeners();
+    }
+
+    /**
+     * @param string $event_name
+     */
+    protected function lazyLoad($event_name)
+    {
+        if (isset($this->listener_ids[$event_name])) {
+            foreach ($this->listener_ids[$event_name] as $service_id) {
+                $listener = $this->container->get($service_id);
+
+                if ($listener instanceof ListenerInterface) {
+                    if (!isset($this->listeners[$event_name][$service_id]) ||
+                        $listener !== $this->listeners[$event_name][$service_id]
+                    ) {
+                        $this->register($event_name, $listener);
+                        $this->listeners[$event_name][$service_id] = $listener;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ tests/Listener/Locator/ContainerAwareLocatorTest.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Pkvs package
+ *
+ * @package Pkvs
+ * @author  Peter Gribanov <pgribanov@1tv.com>
+ */
+namespace GpsLab\Domain\Event\Tests\Listener\Locator;
+
+use GpsLab\Domain\Event\EventInterface;
+use GpsLab\Domain\Event\Listener\ListenerCollection;
+use GpsLab\Domain\Event\Listener\ListenerInterface;
+use GpsLab\Domain\Event\Listener\Locator\ContainerAwareLocator;
+use GpsLab\Domain\Event\NameResolver\EventNameResolverInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ContainerAwareLocatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ContainerAwareLocator
+     */
+    private $locator;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|EventNameResolverInterface
+     */
+    private $resolver;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|ContainerInterface
+     */
+    private $container;
+
+    protected function setUp()
+    {
+        $this->resolver = $this->getMock(EventNameResolverInterface::class);
+        $this->container = $this->getMock(ContainerInterface::class);
+        $this->locator = new ContainerAwareLocator($this->resolver, $this->container);
+    }
+
+    public function testRegister()
+    {
+        /* @var $event EventInterface */
+        $event = $this->getMock(EventInterface::class);
+
+        /* @var $listener1 ListenerInterface */
+        $listener1 = $this->getMock(ListenerInterface::class);
+        $this->locator->register('foo', $listener1);
+
+        /* @var $listener2 ListenerInterface */
+        $listener2 = $this->getMock(ListenerInterface::class);
+        $this->locator->register('foo', $listener2);
+
+        /* @var $listener3 ListenerInterface */
+        $listener3 = $this->getMock(ListenerInterface::class);
+        $this->locator->registerService('bar', 'domain.listener.3');
+
+        /* @var $listener4 ListenerInterface */
+        $listener4 = $this->getMock(ListenerInterface::class);
+        $this->locator->registerService('bar', 'domain.listener.4');
+
+        $this->container
+            ->expects($this->at(0))
+            ->method('get')
+            ->with('domain.listener.3')
+            ->will($this->returnValue($listener3));
+
+        $this->container
+            ->expects($this->at(1))
+            ->method('get')
+            ->with('domain.listener.4')
+            ->will($this->returnValue($listener4));
+
+        // test get list event listeners
+        $listeners = $this->locator->getRegisteredEventListeners();
+        $this->assertInstanceOf(ListenerCollection::class, $listeners);
+        $this->assertEquals(new ListenerCollection([$listener1, $listener2, $listener3, $listener4]), $listeners);
+
+        $this->resolver
+            ->expects($this->atLeastOnce())
+            ->method('getEventName')
+            ->with($event)
+            ->will($this->returnValue('bar'));
+
+        // test get event listeners for event
+        $listeners = $this->locator->getListenersForEvent($event);
+        $this->assertInstanceOf(ListenerCollection::class, $listeners);
+        $this->assertEquals(new ListenerCollection([$listener3, $listener4]), $listeners);
+    }
+
+    public function testNoListenersForEvent()
+    {
+        /* @var $event EventInterface */
+        $event = $this->getMock(EventInterface::class);
+
+        /* @var $listener1 ListenerInterface */
+        $listener1 = $this->getMock(ListenerInterface::class);
+        $this->locator->register('foo', $listener1);
+
+        $this->locator->registerService('foo', 'domain.listener.2');
+
+        $this->container
+            ->expects($this->never())
+            ->method('get');
+
+        $this->resolver
+            ->expects($this->atLeastOnce())
+            ->method('getEventName')
+            ->with($event)
+            ->will($this->returnValue('bar'));
+
+        $listeners = $this->locator->getListenersForEvent($event);
+        $this->assertInstanceOf(ListenerCollection::class, $listeners);
+        $this->assertEquals(new ListenerCollection(), $listeners);
+    }
+}
